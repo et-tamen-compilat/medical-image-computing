092ea05596048ab5a1470b2d596807ff53892fe6
hzm17 <32938442+hzm17@users.noreply.github.com>
2018-02-12 02:19:19 +0000

    Add files via upload
    basic homepage done, need to work on content page

---
+++ environment/javascript/main.js
@@ -0,0 +1,23 @@
+var openingTimeLine = anime.timeline();
+
+openingTimeLine.add({
+  targets: 'path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutCubic',
+  duration: 4000,
+  autoplay: true
+});
+
+openingTimeLine.add({
+    targets: '.circles',
+    opacity: 1,
+    offset:4000,
+    duration: 4000,
+  });
+
+openingTimeLine.add({
+    targets: '#openingTitle',
+    opacity: 1,
+    offset:5000,
+    duration:4000,
+});
