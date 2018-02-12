85dbacf2caff54c13d763e0fec44977a2d80cee0
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-02-12 16:55:11 +0000

    Insert very descriptive commit message

--- environment/javascript/main.js
+++ environment/javascript/main.js
@@ -4,20 +4,20 @@ openingTimeLine.add({
   targets: 'path',
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutCubic',
-  duration: 4000,
+  duration: 4000, //change to 4000
   autoplay: true
 });
 
 openingTimeLine.add({
     targets: '.circles',
     opacity: 1,
-    offset:4000,
-    duration: 4000,
+    offset:4000, //change to 4000
+    duration: 4000, //change to 4000
   });
 
 openingTimeLine.add({
     targets: '#openingTitle',
     opacity: 1,
-    offset:5000,
-    duration:4000,
+    offset:5000, //change to 5000
+    duration:4000, //change to 4000
 });
