bf4a2730d0f6e11d218f8eca9258aac4647533df
Hugo Mayo <hugo.zp.mayo@gmail.com>
2018-02-26 23:35:08 +0000

    slowed animation a little

--- environment/javascript/scripts.js
+++ environment/javascript/scripts.js
@@ -1,20 +1,20 @@
+//causes circles to expand when clicked
 $('a').click(function(e) {
-
   e.preventDefault();
   var img = $(e.currentTarget).children('img');
   img.animate({left: '541px', top:'200px', }, {duration:1000});
   img.toggleClass('circleClicked');
-  setTimeout(function(url) { window.location = url }, 500, this.href);
+  setTimeout(function(url) { window.location = url },1500, this.href);
     
 });
-
+//small expand on hover
 $('a').hover(function(e) {
-
   e.preventDefault();
   var img = $(e.currentTarget).children('img');
   img.toggleClass('circleHover');
-
 });
+
+//content pages fade in
 $(function() {
     $('body').removeClass('fade-out');
 });
