f7ab0c626ae9878c747a9ed02603dfb880afdf6d
Hugo Mayo <hugo.zp.mayo@gmail.com>
2018-02-28 09:55:21 +0000

    testcookie

--- environment/javascript/main.js
+++ environment/javascript/main.js
@@ -1,41 +1,40 @@
 
 $(document).ready(function() {
-     var date = new Date();
- var minutes = 30;
- date.setTime(date.getTime() + (minutes * 60 * 1000));
-   if ($.cookie('hasBeenHere') == null) {
+    
+    if ($.cookie('hasBeenHere') == true) {
           var openingTimeLine = anime.timeline();
-openingTimeLine.add({
-    targets: ['#brain_pic', '#lines'],
-    opacity: 1
-});
-openingTimeLine.add({
-  targets: 'path',
-  opacity: 1,
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutCubic',
-  duration: 4000, //change to 4000
-  autoplay: true
-});
+        openingTimeLine.add({
+            targets: ['#brain_pic', '#lines'],
+            opacity: 1
+        });
+        openingTimeLine.add({
+            targets: 'path',
+            opacity: 1,
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'easeInOutCubic',
+            duration: 4000, //change to 4000
+            autoplay: true
+        });
 
-openingTimeLine.add({
-    targets: ['.circles' ,'.landingLinks'],
-    opacity: 1,
-    offset:4000, //change to 4000
-    duration: 4000, //change to 4000
-  });
+        openingTimeLine.add({
+            targets: ['.circles' ,'.landingLinks'],
+            opacity: 1,
+            offset:4000, //change to 4000
+            duration: 4000, //change to 4000
+        });
 
-openingTimeLine.add({
-    targets: '#openingTitle',
-    opacity: 1,
-    offset:5000, //change to 5000
-    duration:4000, //change to 4000
-});   
+        openingTimeLine.add({
+            targets: '#openingTitle',
+            opacity: 1,
+            offset:5000, //change to 5000
+            duration:4000, //change to 4000
+        });
+    var date = new Date();
+    date.setTime(date.getTime() + (5 * 1000));
     $.cookie('hasBeenHere', true, {expires:date});
-}else{
-    $(".landingLinks, .circles, #lines, #brain_pic, #openingTitle").delay(1000).animate({opacity:1}, 700);
-  
-}
+    }else{
+        $(".landingLinks, .circles, #lines, #brain_pic, #openingTitle").delay(1000).animate({opacity:1}, 700);
+    }
 });
 
 
