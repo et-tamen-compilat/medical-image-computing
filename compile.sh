8b57b31793faa18286bea52b9d845d0da67a3f8e
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-03-14 11:40:14 +0000

    Better HTML & CSS

--- compile.sh
+++ compile.sh
@@ -15,11 +15,11 @@ mkdir -p output
 for file in src/content/*.md; do
     fbname=$(basename "$file" .md)
     echo "Compiling $fbname.md to $fbname.html"
-    pandoc -f markdown -t html --toc --toc-depth 2 --template templates/content.html --mathjax -o "output/$fbname.html" "$file"
+    pandoc -f markdown -t html --toc --toc-depth 2 --template templates/content.html -o "output/$fbname.html" --mathjax "$file"  
 done
 
 for file in src/personal/*.md; do
     fbname=$(basename "$file" .md)
     echo "Compiling $fbname.md to $fbname.html"
-    pandoc -f markdown -t html --template templates/personal.html --mathjax -o "output/$fbname.html" "$file"
+    pandoc -f markdown -t html --template templates/personal.html --mathjax -o "output/$fbname.html" "$file"  
 done
