04ed76dbea1b222b6bede793890c25d6b82593c6
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-03-14 11:41:59 +0000

    Better compile script

--- compile.sh
+++ compile.sh
@@ -12,14 +12,13 @@ cp -r environment/** output/
 cp -r images/** output/content-images/
 mkdir -p output
 
-for file in src/content/*.md; do
-    fbname=$(basename "$file" .md)
-    echo "Compiling $fbname.md to $fbname.html"
-    pandoc -f markdown -t html --toc --toc-depth 2 --template templates/content.html -o "output/$fbname.html" --mathjax "$file"  
-done
+function compile {
+    for file in src/$1/*.md; do
+        fbname=$(basename "$file" .md)
+        echo "Compiling $fbname.md to $fbname.html"
+        pandoc -f markdown -t html --toc --toc-depth 2 --template templates/$1.html -o "output/$fbname.html" --mathjax "$file"  
+    done
+}
 
-for file in src/personal/*.md; do
-    fbname=$(basename "$file" .md)
-    echo "Compiling $fbname.md to $fbname.html"
-    pandoc -f markdown -t html --template templates/personal.html --mathjax -o "output/$fbname.html" "$file"  
-done
+compile "content" "content"
+compile "personal" "personal"
